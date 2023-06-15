b664bc8af69869541ac5806390f064a526c5cf83
Ellis Roberts <ellisrbrts@gmail.com>
2023-06-15 13:29:03 -0700

    Added static compile check to GLObject::CreateBufferObject function

--- src/draw/buffer.h
+++ src/draw/buffer.h
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <cassert>
 #include <GL/glew.h>
+#include <type_traits>
 #include <vector>
 
 //Moved from state: buffer handle is 0
@@ -47,9 +48,12 @@ namespace GLObject
     //Create a buffer from input data to be bound to desired target
     Buffer CreateBufferObject(std::vector<T> const& Data, GLenum Target, GLenum Usage)
     {
+        static_assert(std::is_arithmetic_v<T>);
         Buffer buffer;
         glBindBuffer(Target, buffer.GetHandle());
+        Debug::CheckGLError();
         glBufferData(Target, Data.size()*sizeof(T), Data.data(), Usage);
+        Debug::CheckGLError();
         glBindBuffer(Target, 0);
         return buffer;
     }
